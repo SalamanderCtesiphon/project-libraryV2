0c26b9ffbb017918d0a8c1a3091814148755dce9
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-21 12:49:30 -0500

    added some clases to the display cards in order to add some padding to each section

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function Book(title, author, numberOfPages, haveRead) {
 
 const hobbit = new Book('The Hobbit', 'J.R.R. Tolkein', 295, true);
 const neuromancer = new Book('Neuromancer', 'William Gibson', 271, true);
-const bladerunner = new Book('Do Androids Dream of Electric Sheep', 'Philip K. Dick', 230, true);
+const bladerunner = new Book('Bladerunner', 'Philip K. Dick', 230, true);
 
 
 
@@ -28,14 +28,42 @@ addBookToLibrary(bladerunner);
 
 console.log(myLibrary);
 
-const bookshelf = document.querySelector('.bookshelf');
-const card = document.createElement('div');
-card.setAttribute('class', 'card');
-card.textContent = 'Hello Bitches!'
-bookshelf.appendChild(card);
 
-const displayBooks = function(myLibrary) {
-  return myLibrary.map((book) => {
 
-  })
-}
+
+
+const displayBooks = function() {
+  const bookshelf = document.querySelector('.bookshelf');
+  
+  myLibrary.map((book) => {
+    const card = document.createElement('div');
+    card.setAttribute('class', 'card');
+
+    const bookHeader = document.createElement('h2');
+    bookHeader.textContent = book.title;
+
+    const bookAuthor = document.createElement('h4');
+    bookAuthor.textContent = `Author: ${book.author}`;
+
+    const bookLength = document.createElement('p');
+    bookLength.textContent = `Number of Pages: ${book.numberOfPages}`;
+    bookLength.setAttribute('class', 'num')
+
+    const readBook = document.createElement('p');
+    if(book.haveRead){
+      readBook.textContent = 'Have Read? Yes';
+    } else {
+      readBook.textContent = 'Have Read? No';
+    }
+    card.appendChild(bookHeader);
+    card.appendChild(bookAuthor);
+    card.appendChild(bookLength);
+    card.appendChild(readBook);
+    bookshelf.appendChild(card);
+
+
+
+  });
+};
+
+displayBooks();
