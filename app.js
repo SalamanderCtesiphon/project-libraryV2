6dd40cbbf63e81ba2863073f724f7e57a8421fae
salamanderctesiphon <swbrookshire@gmail.com>
2023-06-21 11:29:49 -0500

    created three book objects

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 let myLibrary = [];
 
-function Book(title) {
+function Book(title, author, numberOfPages, haveRead) {
   //the constructor
   this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.haveRead = haveRead;
 }
 
+const hobbit = new Book('The Hobbit', 'J.R.R. Tolkein', 295, true);
+const neuromancer = new Book('Neuromancer', 'William Gibson', 271, true);
+const bladerunner = new Book('Do Androids Dream of Electric Sheep', 'Philip K. Dick', 230, true);
 
-function addBookToLibrary() {
+
+
+function addBookToLibrary(book) {
   //do stuff here
-}
+  return myLibrary.push(book);
+};
+
+addBookToLibrary(hobbit);
+addBookToLibrary(neuromancer);
+addBookToLibrary(bladerunner);
+
+console.log(myLibrary);
